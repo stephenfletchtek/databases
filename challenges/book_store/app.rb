69a6fca0cb3cb186d19b59b8cd83fedef7bb2b46
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-02 11:44:08 +0100

    app.rb working as expected

--- challenges/book_store/app.rb
+++ challenges/book_store/app.rb
@@ -4,10 +4,10 @@ require_relative 'lib/database_connection'
 DatabaseConnection.connect('book_store')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
+sql = 'SELECT * FROM books;'
 result = DatabaseConnection.exec_params(sql, [])
 
 # Print out each record from the result set .
 result.each do |record|
-  p record
+  p "#{record['id']} - #{record['title']} - #{record['author_name']}"
 end
