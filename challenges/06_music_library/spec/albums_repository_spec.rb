7974132b968ea3de97b539ac0ed4f8a17ed3e160
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-03 14:42:13 +0100

    06 music library copied from 04 music library

---
+++ challenges/06_music_library/spec/albums_repository_spec.rb
@@ -0,0 +1,43 @@
+require 'album_repository'
+
+RSpec.describe AlbumRepository do
+
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds_albums.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+  
+  before(:each) do
+    reset_albums_table
+  end
+
+  it 'returns all albums and their information' do
+    repo = AlbumRepository.new
+
+    albums = repo.all
+
+    expect(albums.length).to eq 2
+
+    expect(albums[0].id).to eq '1'
+    expect(albums[0].title).to eq 'Motomami'
+    expect(albums[0].release_year).to eq '2022'
+    expect(albums[0].artist_id).to eq '1'
+
+    expect(albums[1].id).to eq '2'
+    expect(albums[1].title).to eq 'In Colour'
+    expect(albums[1].release_year).to eq '2015'
+    expect(albums[1].artist_id).to eq '2'
+  end
+
+  it 'returns an album based on an id' do
+    repo = AlbumRepository.new
+
+    album = repo.find('1')
+
+    expect(album.id).to eq '1'
+    expect(album.title).to eq 'Motomami'
+    expect(album.release_year).to eq '2022'
+    expect(album.artist_id).to eq '1'
+  end
+end
