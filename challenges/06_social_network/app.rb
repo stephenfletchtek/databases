b92fde27c5a92bb83794916a015035f9cf14e6c9
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-04 15:04:11 +0100

    configure app.rb to show contents of both tables

--- challenges/06_social_network/app.rb
+++ challenges/06_social_network/app.rb
@@ -1,13 +1,23 @@
 require_relative 'lib/database_connection'
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('social_network')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
+sql = 'SELECT * FROM posts;'
+posts = DatabaseConnection.exec_params(sql, [])
 
-# Print out each record from the result set .
-result.each do |record|
-  p record
+sql = 'SELECT * FROM user_accounts;'
+users = DatabaseConnection.exec_params(sql, [])
+
+posts.each do |record|
+  str = "#{record['id']} - #{record['title']} - #{record['content']} - "
+  str += "#{record['num_views']} - #{record['user_account_id']}"
+  puts str
+end
+
+puts ""
+
+users.each do |record|
+  puts "#{record['id']} - #{record['email_address']} - #{record['username']}"
 end
