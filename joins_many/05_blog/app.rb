1ac909343a6450f4d25b849b20565c2c3372c790
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-06 17:58:22 +0100

    app working for posts and tags

--- joins_many/05_blog/app.rb
+++ joins_many/05_blog/app.rb
@@ -1,4 +1,5 @@
 require_relative 'lib/post_repo'
+require_relative 'lib/tag_repo'
 require_relative 'lib/database_connection'
 
 DatabaseConnection.connect('blog_2')
@@ -6,6 +7,17 @@ DatabaseConnection.connect('blog_2')
 repo = PostRepository.new
 results = repo.find_by_tag(1)
 
+puts "posts with tag '1'"
 results.each do |post|
   puts "#{post.id}. #{post.title}"
+end
+
+puts ""
+
+repo = TagRepository.new
+results = repo.find_by_post(6)
+
+puts "tags on post '6'"
+results.each do |tag|
+  puts "#{tag.id}. #{tag.name}"
 end
